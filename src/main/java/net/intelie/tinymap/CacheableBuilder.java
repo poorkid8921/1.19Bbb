ada6a749c05852c96d79b9d4b15b7c78b3d1c8f2
Floop <76122284+poorkid8921@users.noreply.github.com>
2023-08-27 20:02:33 +0300

    Delete src/main/java/net/intelie/tinymap directory

--- src/main/java/net/intelie/tinymap/CacheableBuilder.java
+++
@@ -1,7 +0,0 @@
-package net.intelie.tinymap;
-
-public interface CacheableBuilder<B, T> {
-    T build();
-
-    CacheAdapter<B, T> adapter();
-}
--- src/main/java/net/intelie/tinymap/base/IndexedSet.java
+++
@@ -1,6 +0,0 @@
-package net.intelie.tinymap.base;
-
-import java.util.Set;
-
-public interface IndexedSet<T> extends Set<T>, IndexedCollection<T> {
-}
